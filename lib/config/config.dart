33291aa80307aca3cd36475cebe6d4ab7d076c86
campovski <campovski@gmail.com>
2020-10-25 19:21:31 +0100

    SLFIP-2: Change Config.maxMoves from 100 to 150

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,6 +1,6 @@
 abstract class Config {
   /// Initial max moves in a game, can be made bigger with watching videos.
-  static const int maxMoves = 100;
+  static const int maxMoves = 150;
 
   /// Maximum number of seconds a player can spend in a game. Can be made
   /// bigger by watching videos.
